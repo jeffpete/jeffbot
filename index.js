30280a0a11bbb85165d46fc33cbdd380b535f04c
Glitch (jeffpete-bot) <none>
2018-06-27 20:28:30 +0000

    no

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ client.on('message', (message) => {
 
 client.on('message', (message) => {
   if(message.content == '!jeff help') {
-    message.channel.sendMessage('https://pastebin.com/sdJsUjXH');
+    message.channel.sendMessage('https://pastebin.com/raw/sdJsUjXH');
   }
 });
 
